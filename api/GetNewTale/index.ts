f46809fc0ae40bcb9dc12f0b3244a5d10ec3f31e
Jirachai Chansivanon <jirachai.c@outlook.com>
2023-05-09 23:38:06 +0700

    🛠 Fix text animation, Add GetNewTale function

--- api/GetNewTale/index.ts
+++ api/GetNewTale/index.ts
@@ -5,11 +5,11 @@ async function requestForCompletion(userInput: string) {
     const OaiApiKey = process.env.OAI_API_KEY || 'NOT_FOUND'
     const MAX_TOKEN = 1000
 
-    const url = 'https://jirachai-openai.openai.azure.com/openai/deployments/chatgpt-turbo-001/chat/completions?api-version=2023-03-15-preview'
+    const url = process.env.OAI_API_HOST || 'NOT_FOUND'
     const prompts = [
         {
             role: 'system',
-            content: 'You are a story teller and tales author. You write stories and tales for children. You write in a friendly yet professional tone but can tailor your writing style that best works for a user-specified audience. If you do not know the answer to a question, respond by saying "I do not know the answer to your question, I am just a story teller." in the input language. With total token not over than ' + MAX_TOKEN + ' tokens'
+            content: 'You are a story teller and tales author. You write stories and tales for adult and children. You write in a friendly yet professional tone but can tailor your writing style that best works for a user-specified audience and provide a smooth ending. If you do not know the answer to a question, respond by saying "I do not know the answer to your question, I am just a story teller.". With total token not over than ' + MAX_TOKEN + ' tokens. And please answer in the input language'
         }
     ]
 
@@ -19,7 +19,7 @@ async function requestForCompletion(userInput: string) {
     })
 
     const response = await axios.post(url, {
-            messages: JSON.stringify(prompts),
+            messages: prompts,
             max_tokens: 1000,
             temperature: 1,
             frequency_penalty: 0,
@@ -33,35 +33,42 @@ async function requestForCompletion(userInput: string) {
         },
     }).catch((error) => {
         if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data)
-        console.log(error.response.status)
-        console.log(error.response.headers)
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      console.log(error.request)
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', error.message)
-    }
-    console.log(error.config)
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data.error.message)
+            throw error.response.data.error.message
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request)
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            throw new Error('Error ' + error.message)
+        }
+        console.log(error.config)
     })
 
-    return response
+    if (response) {
+        return response.data.choices[0].message.content
+    }
+    throw new Error('Cannot get response from OpenAI API')
 }
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    // const responseMessage = faker.name.fullName()
-    const inputMessage = 'หมูน้อย'
+    const inputMessage = req.query.inputMessage || (req.body && req.body.inputMessage)
     const responseMessage = await requestForCompletion(inputMessage)
+        .catch((error) => {
+            context.res = {
+                status: 400,
+                body: error.message,
+            }
+            return
+        })
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
         body: responseMessage
-    };
+    }
 
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { createTale } from './utils/oai'
 interface ITypingText {
   children: string
   cutAmount?: number
+  typingSpeed?: number
 }
 
 function TypingText(props: ITypingText) {
   const [isTyping, setIsTyping] = useState(false)
   const [text, setText] = useState('')
   const [textArray, setTextArray] = useState<Array<string>>([])
+  const cutAmount = props.cutAmount || 1
+  const speed = props.typingSpeed || 50
 
   useEffect(() => {
-    if (!isTyping || text === '') {
+    if (!isTyping && text === '') {
       setIsTyping(true)
       setTextArray(props.children.split(''))
     }
@@ -24,12 +27,12 @@ function TypingText(props: ITypingText) {
     }
   }, [textArray, isTyping, text])
 
-  function typingAnimation(speed = 50, cutAmount = 1) {
+  function typingAnimation() {
     const _textArray = textArray
 
     setTimeout(() => {
       if (textArray.length > 0) {
-        const t = _textArray.splice(0, cutAmount)
+        const t = _textArray.splice(0, cutAmount).join('')
 
         setText(prevText =>  prevText.concat(t))
         setTextArray(_textArray)
@@ -44,18 +47,29 @@ function TypingText(props: ITypingText) {
   )
 }
 
+// App
+
 function App() {
   const [response, setResponse] = useState('')
   const [request, setRequest] = useState('')
   const [tale, setTale] = useState('')
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
+  const [isMount, setIsMount] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsMount(true)
+    }, 10)
+  }, [])
 
   useEffect(() => {
     setIsLoading(false)
   }, [tale])
 
   async function submit() {
+    setTale('')
+    setError(null)
     setIsLoading(true)
     try {
       const responseTale = await createTale(request)
@@ -68,11 +82,6 @@ function App() {
     }
   }
 
-  async function trySubmit() {
-    setTale('')
-    setError(null)
-    setResponse(request)
-  }
 
   // หมี ไปสอบ IELTS
 
@@ -80,28 +89,26 @@ function App() {
     <>
       <div className="flex flex-col justify-center h-full px-4">
         <div>
-          <h1 className="text-center font-bold">GPT! Tell me the story</h1>
+          <h1 className="text-center text-7xl font-bold">GPT! Tell me the story</h1>
         </div>
 
-        <div className="text-center">
-          <p className="text-xl"><TypingText>Ok my  dear, what kind of story do you want to hear?</TypingText></p>
+        <div className={`text-center delay-500 duration-1000 transition-all ${isMount ? 'opacity-100' : 'opacity-0'}`}>
+          <p className="text-2xl"><TypingText>Ok my  dear, what kind of story do you want to hear?</TypingText></p>
         </div>
 
-        {
-          !isLoading && (
-            <div className="w-full text-center mt-4">
-              <p className="">I would like to hear the story about...</p>
-              <div className="mt-2">
-                <input type="text" className="px-2 py-2 rounded-l-md bg-black/20" onChange={e => setRequest(e.target.value)} />
-                <button className="px-2 py-2 bg-amber-800 rounded-r-md" onClick={submit}>Tell me!</button>
-              </div>
+        <div className={`transition-all duration-1000 w-full ${isLoading ? '-translate-y-24 opacity-0' : 'translate-y-0 opacity-100'}`}>
+          <div className={`w-full text-center mt-12 transition-all duration-1000 delay-1000 ${isMount ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}>
+            <p className="text-xl">I would like to hear the story about...</p>
+            <div className="mt-2">
+              <input type="text" className="px-2 py-2 rounded-l-md bg-black/20 text-xl" onChange={e => setRequest(e.target.value)} />
+              <button className="px-2 py-2 bg-amber-800 rounded-r-md text-xl" onClick={submit}>Tell me!</button>
             </div>
-          )
-        }
+          </div>
+        </div>
 
         {
           isLoading && (
-            <div className="text-center text-xl font-bold animate-pulse my-4">
+            <div className="text-center text-4xl font-bold animate-pulse my-4">
               <p>Ok, let me think...</p>
             </div>
           )
@@ -109,10 +116,10 @@ function App() {
 
         {
           !isLoading && (error !== null || tale !== '') && (
-            <div className="mt-8 text-center mx-auto lg:w-2/3 bg-white/10 p-4 rounded-md">
+            <div className={`mt-8 text-center mx-auto lg:w-2/3 p-4 rounded-md ${error !== null ? 'bg-red-500/25' : 'bg-white/10'}`}>
 
-              <p className="text-xl text-red-400">
-                {error !== null && <TypingText>{error}</TypingText>}
+              <p className="text-xl text-red-300 my-2">
+                {error !== null && <TypingText typingSpeed={25} cutAmount={5}>{error}</TypingText>}
               </p>
 
               <p className="text-xl">
